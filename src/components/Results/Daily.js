c81f6d2a682b9f84b5d1b21b572705652f19ed66
Aitor <aitorulz1@gmail.com>
2021-01-28 12:33:05 +0100

    allowing arrows

--- src/components/Results/Daily.js
+++ src/components/Results/Daily.js
@@ -46,8 +46,8 @@ export default function Daily(city) {
                 requestToChangeActive={setActiveItemIndex}
                 activeItemIndex={activeItemIndex}
                 numberOfCards={5}
-                //leftChevron={<button className="arrow" >{'<'}</button>}
-                //rightChevron={<button className="arrow" >{'>'}</button>}
+                leftChevron={<button className="arrow" >{'<'}</button>}
+                rightChevron={<button className="arrow" >{'>'}</button>}
                 outsideChevron
                 chevronWidth={40}
                 slidesToScroll={5}
